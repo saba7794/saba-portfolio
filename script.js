7e1a31632aad5827e4edce0d965a582293a6a04d
SABA NAZ <146808827+saba7794@users.noreply.github.com>
2025-08-17 15:27:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for nav (if you add a navbar later)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
